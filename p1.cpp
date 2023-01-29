23b867c28e5e514dd36c1b277c81624b2d1273fe
Wootak Kim <wootakk1@uci.edu>
2023-01-29 11:47:50 +0900

    Practice C++

--- p1.cpp
+++ p1.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    float BS,PA,PD;
+
+    std::cout << "Basic Salary\n";
+    std::cin >> BS;
+    std::cout << "Area is " << a << std::endl;
 }
