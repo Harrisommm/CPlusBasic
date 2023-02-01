0941b851f11bf7a55cf9631015c8118697f09c53
Wootak Kim <wootakk1@uci.edu>
2023-02-01 22:37:02 +0900

    nested for loop patterns

--- p1.cpp
+++ p1.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 
 int main()
-{
-    /*
-    int x = 4, y = 5;
-    if(x>y)
+{   
+ 
+
+    
+    for(int i=0; i<5; i++)
     {
-        std::cout << "x is larger" << std::endl;
+        for(int j=0; j<5; j++)
+        {
+            if((i+j)<=4)
+            {
+                std::cout << "* ";
+            }
+            else
+            {
+                std::cout << "  ";
+            }
+        }
+        std::cout << std::endl;
     }
-    else if (x<y)
-    {
-        std::cout << "y is larger" << std::endl;
-    }
-    else
-    {
-        std::cout << "equal" << std::endl;
-    }
-    */
-    int test = 10/3;
-    std::cout << test;
-    std::cout << "Hello are you there";
+
+    //Draw Pattern given in description for n x n dimensions
+
 
     return 0;
 }
