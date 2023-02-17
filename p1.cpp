52d8663cd3b3d5d9771ec3aac1fe357ce21c5990
Harrison Kim <wootak95@gmail.com>
2023-02-17 16:36:58 +0900

    Class practice

--- p1.cpp
+++ p1.cpp
@@ -80,7 +80,7 @@ int main()
 
     return 0;
 }
-*/
+
 int Search(int A[], int n, int key)
 {
     for(int i=0; i<n; i++)
@@ -92,16 +92,70 @@ int Search(int A[], int n, int key)
     
     }
 }
+*/
+class Student
+{
+private:
+    int Roll;
+    std::string name;
+    int x,y,z;
+    
+public:
+    Student(int R, std::string n, int a, int b, int c)
+    {
+        Roll = R;
+        name = n;
+        x = a;
+        y = b;
+        z = c;
+    }
+    int total()
+    {
+        return x + y + z;
+    }
+    char grade()
+    {
+        float avg = total()/3;
+        if(avg > 60)
+        {
+            return 'A';
+        }
+        else if(avg <60 && avg >=40)
+        {
+            return 'B';
+        }
+        else
+        {
+            return 'C';
+        }
+    }
+    int getRoll()
+    {
+        return Roll;
+    }
+    std::string getName()
+    {
+        return name;
+    }
+};
 int main()
 {
-	int A[]={2,4,5,7,10,9,13};
-    int k;
-    std::cout<<"Enter an Element to be Searched:";
-    std::cin>>k;
-    int index=Search(A,7,k);
+    int R;
+    std::string n;
+    int x=0,y=0,z=0;
 
-    std::cout<<"Element found at index :"<<index<<std::endl;
-    return 0;
+    std::cout << "Roll No" << std::endl;
+    std::cin >> R;
+    std::cout << "Name" << std::endl;
+    std::cin >> n;
+    std::cout << "3 grades" << std::endl;
+    std::cin >> x >> y >> z;
+
+    Student s(R,n,x,y,z);
+    std::cout << "Roll No:" << s.getRoll() << s.getName() << std::endl;
+
+    std::cout << "Total grade is " << s.total() << std::endl;
+    std::cout << "Letter grade is " << s.grade() << std::endl;
 }
 
 
